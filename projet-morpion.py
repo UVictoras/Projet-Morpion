8670b855a9ecfad0b3a45e931b24aca6a1d2917f
VictorMartinan <vmartinan@gaming.tech>
2022-11-14 15:21:44 +0100

    Définitions des classes

--- projet-morpion.py
+++ projet-morpion.py
@@ -2,14 +2,34 @@
 
 ' --- Importation du ou des modules --- '
 
+import random as r
 
+' --- Définition de la classe Player --- '
 
-' --- Définition de la classe Morpion --- '
+class Player():
 
-
+    def __init__(self, name, sign):
+        self.name = name
+        self.sign = sign
 
 ' --- Définition de la classe IA --- '
 
+class IA():
 
+    pass
+
+' --- Définition de la classe Morpion --- '
+
+class Morpion():
+
+    def __init__(self):
+        self.grid = []
+
+    def PrepareGrid(self):
+        for i in range(3):
+            row = []
+            for i in range(3):
+                row.append('-')
+            self.grid.append(row)
 
 ''' FIN '''

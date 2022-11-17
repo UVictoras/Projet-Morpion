c2168ee498e0e77e861db700ec7fc9c91f88de94
VictorMartinan <vmartinan@gaming.tech>
2022-11-17 18:41:47 +0100

    Ajouts

--- projet-morpion.py
+++ projet-morpion.py
@@ -23,16 +23,18 @@ class IA():
 
 ' --- Définition de la classe Morpion --- '
 
-class Morpion(Player, IA):
+class Morpion():
 
     def __init__(self):
         self.grid = []
 
     def PrepareGrid(self):
-        row1 = ['1 1', '1 2', '1 3']
-        row2 = ['2 1', '2 2', '2 3']
-        row3 = ['3 1', '3 2', '3 3']
-        self.grid.append(row1, row2, row3)
+        row1 = ["-", "-", "-"]
+        row2 = ["-", "-", "-"]
+        row3 = ["-", "-", "-"]
+        self.grid.append(row1)
+        self.grid.append(row2)
+        self.grid.append(row3)
 
 
     def PrintGrid(self):
@@ -40,19 +42,45 @@ class Morpion(Player, IA):
             print(self.grid[i])
     
     def VerifyEmptyCase(self, x, y):
-        if self.grid[x-1][y-1] == 'X' or self.grid[x-1][y-1] == 'O':
+        if self.grid[x-1][y-1] == "X" or self.grid[x-1][y-1] == "O":
             return False
         else:
             return True
 
     def UpdateGrid(self, x, y):
-        if self.VerifyEmptyCase(self, x, y):
+        if self.VerifyEmptyCase(x, y):
             if Player.sign == 'X':
                 self.grid[x-1][y-1] = 'X'
             else:
                 self.grid[x-1][y-1] = 'O'
         else:
             print("Choose another case please, this one is already full.")
+
+    def VerifyLine(self):
+        for i in range(3):
+            if self.grid[i][0] == 'X' and self.grid[i][1] == 'X' and self.grid[i][2] == 'X':
+                print("The winner is Player 1 ! Congratulations {} !".format(Player1.name))
+
+
+' --- Commandes pour jouer une partie ---'
+
+turn = 0
+GameTicTacToe = Morpion()
+GameTicTacToe.PrepareGrid()
+Player1 = Player('Victor', 'X')
+Player2 = Player('Benjamin', 'O')
+while turn < 9:
+    GameTicTacToe.PrintGrid()
+    if turn%2 == 0:
+        Player1ChoiceX = input("Which line do you choose, {} : ".format(Player1.name))
+        Player1ChoiceY = input("Which column do you choose, {} : ".format(Player1.name))
+        GameTicTacToe.UpdateGrid(Player1ChoiceX, Player1ChoiceY)
+    else:
+        Player2ChoiceX = input("Which line do you choose, {} : ".format(Player2.name))
+        Player2ChoiceY = input("Which column do you choose, {} : ".format(Player2.name))
+        GameTicTacToe.UpdateGrid(Player2ChoiceX, Player2ChoiceY)
+
+
 
 
 ''' FIN '''

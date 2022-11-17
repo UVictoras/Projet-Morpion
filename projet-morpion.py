7508367efb5ecd45039754113200df2e25810a8b
VictorMartinan <vmartinan@gaming.tech>
2022-11-17 19:01:17 +0100

    verify

--- projet-morpion.py
+++ projet-morpion.py
@@ -23,15 +23,15 @@ class IA():
 
 ' --- Définition de la classe Morpion --- '
 
-class Morpion():
+class Morpion(Player):
 
     def __init__(self):
         self.grid = []
 
     def PrepareGrid(self):
-        row1 = ["-", "-", "-"]
-        row2 = ["-", "-", "-"]
-        row3 = ["-", "-", "-"]
+        row1 = ['-', '-', '-']
+        row2 = ['-', '-', '-']
+        row3 = ['-', '-', '-']
         self.grid.append(row1)
         self.grid.append(row2)
         self.grid.append(row3)
@@ -42,12 +42,12 @@ class Morpion():
             print(self.grid[i])
     
     def VerifyEmptyCase(self, x, y):
-        if self.grid[x-1][y-1] == "X" or self.grid[x-1][y-1] == "O":
+        if self.grid[x-1][y-1] == 'X' or self.grid[x-1][y-1] == 'O':
             return False
         else:
             return True
 
-    def UpdateGrid(self, x, y):
+    def UpdateGrid(self, x, y, Player):
         if self.VerifyEmptyCase(x, y):
             if Player.sign == 'X':
                 self.grid[x-1][y-1] = 'X'
@@ -60,27 +60,50 @@ class Morpion():
         for i in range(3):
             if self.grid[i][0] == 'X' and self.grid[i][1] == 'X' and self.grid[i][2] == 'X':
                 print("The winner is Player 1 ! Congratulations {} !".format(Player1.name))
+                winner = 'Player1'
+            elif self.grid[i][0] == 'O' and self.grid[i][1] == 'O' and self.grid[i][2] == 'O':
+                print("The winner is Player 2 ! Congratulations {} !".format(Player2.name))
+                winner  = 'Player2'
 
+    def VerifyColumn(self):
+        for i in range(3):
+            if self.grid[0][i] == 'X' and self.grid[1][i] == 'X' and self.grid[2][i] == 'X':
+                print("The winner is Player 1 ! Congratulations {} !".format(Player1.name))
+                winner = 'Player1'
+            elif self.grid[0][i] == 'O' and self.grid[1][i] == 'O' and self.grid[2][i] == 'O':
+                print("The winner is Player 2 ! Congratulations {} !".format(Player2.name))
+                winner  = 'Player2'
+
+    def VerifyDiagonal(self):
+        if (self.grid[0][0] == 'X' and self.grid[1][1] == 'X' and self.grid[2][2] == 'X') or (self.grid[0][2] == 'X' and self.grid[1][1] == 'X' and self.grid[2][0] == 'X'):
+            print("The winner is Player 1 ! Congratulations {} !".format(Player1.name))
+            winner = 'Player1'
+        elif (self.grid[0][0] == 'O' and self.grid[1][1] == 'O' and self.grid[2][2] == 'O') or (self.grid[0][2] == 'O' and self.grid[1][1] == 'O' and self.grid[2][0] == 'O'):
+            print("The winner is Player 2 ! Congratulations {} !".format(Player2.name))
+            winner  = 'Player2'
 
 ' --- Commandes pour jouer une partie ---'
 
 turn = 0
-GameTicTacToe = Morpion()
-GameTicTacToe.PrepareGrid()
 Player1 = Player('Victor', 'X')
 Player2 = Player('Benjamin', 'O')
+GameTicTacToe = Morpion()
+GameTicTacToe.PrepareGrid()
 while turn < 9:
     GameTicTacToe.PrintGrid()
     if turn%2 == 0:
-        Player1ChoiceX = input("Which line do you choose, {} : ".format(Player1.name))
-        Player1ChoiceY = input("Which column do you choose, {} : ".format(Player1.name))
-        GameTicTacToe.UpdateGrid(Player1ChoiceX, Player1ChoiceY)
+        Player1ChoiceX = int(input("Which line do you choose, {} : ".format(Player1.name)))
+        Player1ChoiceY = int(input("Which column do you choose, {} : ".format(Player1.name)))
+        GameTicTacToe.UpdateGrid(Player1ChoiceX,Player1ChoiceY, Player1)
+        turn += 1
+
     else:
-        Player2ChoiceX = input("Which line do you choose, {} : ".format(Player2.name))
-        Player2ChoiceY = input("Which column do you choose, {} : ".format(Player2.name))
-        GameTicTacToe.UpdateGrid(Player2ChoiceX, Player2ChoiceY)
-
-
+        Player2ChoiceX = int(input("Which line do you choose, {} : ".format(Player2.name)))
+        Player2ChoiceY = int(input("Which column do you choose, {} : ".format(Player2.name)))
+        GameTicTacToe.UpdateGrid(Player2ChoiceX, Player2ChoiceY, Player2)
+        turn += 1
+    print("")
+GameTicTacToe.PrintGrid()
 
 
 ''' FIN '''
